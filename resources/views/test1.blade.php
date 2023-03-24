1631be278a5ae9ba9d8bfcbd0424e7e802942564
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-03-24 17:06:43 +0530

    Day end commit

---
+++ resources/views/test1.blade.php
@@ -0,0 +1,7 @@
+<h1>Welcome to test1 page</h1>
+
+<?php echo $foo; ?>
+
+{{ $foo}}
+
+{{ $haystack}}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/test1", function () {
+    return view("test1", [
+        "foo" => "bar1",
+        "haystack" => "needle",
+    ]);
+});
+
+Route::get("/test2", function () {
+    return view("test2");
 });
