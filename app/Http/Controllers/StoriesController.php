6fd64f77866c5c86f59c9a862624f2322aba1d69
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-03-29 15:59:36 +0530

    StoriesController init

---
+++ app/Http/Controllers/StoriesController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Story;
+use Illuminate\Http\Request;
+
+class StoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $stories = Story::where("user_id", auth()->user()->id)
+            ->orderBy("id", "DESC")
+            ->paginate(2);
+        return view("stories.index", [
+            "stories" => $stories,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("stories.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Story $story)
+    {
+        return view("stories.show", [
+            "story" => $story,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Story $story)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Story $story)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Story $story)
+    {
+        //
+    }
+}
